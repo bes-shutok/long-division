e4b51ba39535b036a864b2a33a97e81d2a683dff
Andrey Dmitriev <dmitriev.andrey.vitalyevich@gmail.com>
2021-08-17 12:00:23 +0100

    Facade for the Long Division

--- src/main/java/ua/com/foxminded/division/LongDivisionFacade.java
+++ src/main/java/ua/com/foxminded/division/LongDivisionFacade.java
@@ -1,5 +1,19 @@
 package ua.com.foxminded.division;
 
+import ua.com.foxminded.division.calculator.LongDivisionCalculator;
+import ua.com.foxminded.division.formatter.LongDivisionFormatter;
+
 public class LongDivisionFacade {
 
+    private final LongDivisionCalculator longDivisionCalculator;
+    private final LongDivisionFormatter longDivisionFormatter;
+    
+    public LongDivisionFacade(LongDivisionCalculator longDivisionCalculator, LongDivisionFormatter longDivisionFormatter) {
+	this.longDivisionCalculator = longDivisionCalculator;
+	this.longDivisionFormatter = longDivisionFormatter;
+    }
+    
+    String longDivision(int numerator, int denominartor) {
+	return longDivisionFormatter.format(longDivisionCalculator.calculate(numerator, denominartor));
+    }
 }
--- src/main/java/ua/com/foxminded/division/calculator/LongDivisionCalculator.java
+++ src/main/java/ua/com/foxminded/division/calculator/LongDivisionCalculator.java
@@ -8,19 +8,17 @@ public class LongDivisionCalculator implements Calculator {
 
     @Override
     public LongDivision calculate(int numerator, int denominator) {
-	String numeratorString = String.valueOf(numerator);	
-	int numeratorStart = 0;
-	
 	if (denominator == 0) {
 	    throw new IllegalArgumentException(DENOMINATOR_ZERO);
 	}
-	
+
+	String numeratorString = String.valueOf(numerator);	
+	int numeratorStart = 0;
 	int quotient = numerator / denominator;
 	char[] qChars= String.valueOf(quotient).toCharArray();	
 	int[] perStepMinuends = new int[qChars.length];
 	int[] perStepSubtrahends = new int[qChars.length];
 	int[] perStepDifferences = new int[qChars.length];
-	
 	int longDivisionStep = 0;
 	String previouseStepDifferenceString = "";
 	
@@ -36,11 +34,7 @@ public class LongDivisionCalculator implements Calculator {
 		int stepDifference = stepMinuend - stepSubtrahend;
 		perStepDifferences[longDivisionStep] = stepDifference;
 		previouseStepDifferenceString = stepDifference == 0 ? "" : String.valueOf(stepDifference);
-		if (stepDifference == 0) {
-		numeratorStart += String.valueOf(stepMinuend).length();
-		} else {
-		    numeratorStart += String.valueOf(stepDifference).length();
-		}
+		numeratorStart += stepDifference == 0 ? length(stepMinuend) : length(stepDifference);
 		longDivisionStep++;
 	    }
 	}
@@ -49,9 +43,9 @@ public class LongDivisionCalculator implements Calculator {
 	perStepDifferences = Arrays.copyOf(perStepDifferences, longDivisionStep);
 	return new LongDivision(numerator, denominator, quotient, perStepMinuends, perStepSubtrahends, perStepDifferences);
     }
-    
+
     private int getStepMinuend(String previouseStepDifferenceString, String numeratorLeftoverString, int stepSubtrahend) {
-	int stepSubtrahendLength = String.valueOf(stepSubtrahend).length();
+	int stepSubtrahendLength = length(stepSubtrahend);
 	int minuendLeftoverLength = stepSubtrahendLength - previouseStepDifferenceString.length();
 	int thisStepMinuendCandidate = Integer.parseInt(previouseStepDifferenceString + numeratorLeftoverString.substring(0, minuendLeftoverLength));
 	if (thisStepMinuendCandidate - stepSubtrahend >= 0) {
@@ -59,5 +53,9 @@ public class LongDivisionCalculator implements Calculator {
 	} else {
 	    return Integer.parseInt(numeratorLeftoverString.substring(0, minuendLeftoverLength + 1));
 	}
+    }
+    
+    private int length(int number) {
+	return String.valueOf(number).length();
     }
 }
