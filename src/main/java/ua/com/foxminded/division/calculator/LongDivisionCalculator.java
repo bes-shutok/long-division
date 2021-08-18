fc10cdb3c2f537015a7960cc3ae95f504a4ece5b
Andrey Dmitriev <dmitriev.andrey.vitalyevich@gmail.com>
2021-08-18 14:14:50 +0100

    Code Review changes - shortened calculate method

--- src/main/java/ua/com/foxminded/division/calculator/LongDivisionCalculator.java
+++ src/main/java/ua/com/foxminded/division/calculator/LongDivisionCalculator.java
@@ -1,50 +1,46 @@
 package ua.com.foxminded.division.calculator;
 
-import java.util.Arrays;
-
 import ua.com.foxminded.division.dto.LongDivision;
 
 public class LongDivisionCalculator implements Calculator {
 
     @Override
     public LongDivision calculate(int numerator, int denominator) {
-	if (denominator == 0) {
-	    throw new IllegalArgumentException(DENOMINATOR_ZERO);
-	}
-
+	validate(denominator);
 	String numeratorString = String.valueOf(numerator);	
 	int numeratorStart = 0;
 	int quotient = numerator / denominator;
-	char[] qChars= String.valueOf(quotient).toCharArray();	
-	int[] perStepMinuends = new int[qChars.length];
-	int[] perStepSubtrahends = new int[qChars.length];
-	int[] perStepDifferences = new int[qChars.length];
-	int longDivisionStep = 0;
-	String previouseStepDifferenceString = "";
-	
-	for (int i = 0; i < qChars.length; i++) {
+	int steps = String.valueOf(quotient).replaceAll("0", "").length();
+	char[] qChars = String.valueOf(quotient).toCharArray();	
+	int[] perStepMinuends = new int[steps];
+	int[] perStepSubtrahends = new int[steps];
+	int[] perStepDifferences = new int[steps];
+	for (int i = 0, longDivisionStep = 0, stepDifference = 0; i < qChars.length; i++) {
 	    int multiplier = Character.getNumericValue(qChars[i]);
 	    if (multiplier != 0) {
 		int stepSubtrahend = denominator * multiplier;
 		perStepSubtrahends[longDivisionStep] = stepSubtrahend;
 		
-		int stepMinuend = getStepMinuend(previouseStepDifferenceString, numeratorString.substring(numeratorStart), stepSubtrahend);
+		int stepMinuend = getStepMinuend(stepDifference, numeratorString.substring(numeratorStart), stepSubtrahend);
 		perStepMinuends[longDivisionStep] = stepMinuend;
 		
-		int stepDifference = stepMinuend - stepSubtrahend;
+		stepDifference = stepMinuend - stepSubtrahend;
 		perStepDifferences[longDivisionStep] = stepDifference;
-		previouseStepDifferenceString = stepDifference == 0 ? "" : String.valueOf(stepDifference);
 		numeratorStart += stepDifference == 0 ? length(stepMinuend) : length(stepDifference);
 		longDivisionStep++;
 	    }
 	}
-	perStepSubtrahends = Arrays.copyOf(perStepSubtrahends, longDivisionStep);
-	perStepMinuends = Arrays.copyOf(perStepMinuends, longDivisionStep);
-	perStepDifferences = Arrays.copyOf(perStepDifferences, longDivisionStep);
 	return new LongDivision(numerator, denominator, quotient, perStepMinuends, perStepSubtrahends, perStepDifferences);
     }
 
-    private int getStepMinuend(String previouseStepDifferenceString, String numeratorLeftoverString, int stepSubtrahend) {
+    private void validate(int denominator) {
+	if (denominator == 0) {
+	    throw new IllegalArgumentException(DENOMINATOR_ZERO);
+	}
+    }
+
+    private int getStepMinuend(int stepDifference, String numeratorLeftoverString, int stepSubtrahend) {
+	String previouseStepDifferenceString = stepDifference == 0 ? "" : String.valueOf(stepDifference);
 	int stepSubtrahendLength = length(stepSubtrahend);
 	int minuendLeftoverLength = stepSubtrahendLength - previouseStepDifferenceString.length();
 	int thisStepMinuendCandidate = Integer.parseInt(previouseStepDifferenceString + numeratorLeftoverString.substring(0, minuendLeftoverLength));
