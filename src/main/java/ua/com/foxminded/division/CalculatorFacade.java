e3f57439173d33cbdafedc8e7e70edaa58caa7d0
Andrey Dmitriev <dmitriev.andrey.vitalyevich@gmail.com>
2021-08-19 10:18:52 +0100

    facade refactored to be more universal

---
+++ src/main/java/ua/com/foxminded/division/CalculatorFacade.java
@@ -0,0 +1,21 @@
+package ua.com.foxminded.division;
+
+import ua.com.foxminded.division.calculator.Calculator;
+import ua.com.foxminded.division.calculator.LongDivisionCalculator;
+import ua.com.foxminded.division.formatter.Formatter;
+import ua.com.foxminded.division.formatter.LongDivisionFormatter;
+
+public class CalculatorFacade {
+
+    private final Calculator calculator;
+    private final Formatter formatter;
+    
+    public CalculatorFacade(LongDivisionCalculator calculator, LongDivisionFormatter formatter) {
+	this.calculator = calculator;
+	this.formatter = formatter;
+    }
+    
+    String longDivision(int numerator, int denominator) {
+	return formatter.format(calculator.calculate(numerator, denominator));
+    }
+}
